3b2db87d078802c2e3fad8c6d4d627d4d57c9d9a
Susbot <susbot651968498@gmail.com>
2022-01-22 15:47:41 -0700

    section 2

--- search/views.py
+++ search/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+projectslist = [
+    {
+        'id':'1',
+        'title': 'woof Website',
+        'description': 'random one'
+    },
+    {
+        'id':'2',
+        'title': 'quack website',
+        'description': 'random two'
+    },
+    {
+        'id': '3',
+        'title': 'meow website',
+        'description':'random three'
+    },
+]
+
+def projects(request):
+    page = 'projects'
+    number = 10
+    context = {'page':page, 'number': number, 'projects': projectslist}
+    return render(request, 'content/projects.html', context)
+
+def project(request, pk):
+    projectObj = None
+    for i in projectslist:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'content/single.html', {'project': projectObj})
